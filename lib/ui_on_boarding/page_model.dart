c5da5afd5abbe9184e952f67389e8d62bdf1c3e6
FabianVarela <jfabian.303@gmail.com>
2020-05-01 23:38:16 -0500

    Modify model page on boarding

--- lib/ui_on_boarding/page_model.dart
+++ lib/ui_on_boarding/page_model.dart
@@ -1,37 +1,31 @@
 import 'package:flutter/material.dart';
 
 class PageModel {
-  String imageUrl;
+  final String imageUrl;
+  final String title;
+  final String body;
+  final List<Color> titleGradient;
 
-  String title;
-  String body;
-  List<Color> titleGradient = <Color>[];
-
-  PageModel({
-    this.imageUrl,
-    this.title,
-    this.body,
-    this.titleGradient,
-  });
+  PageModel(this.imageUrl, this.title, this.body, this.titleGradient);
 }
 
 List<PageModel> pageList = <PageModel>[
   PageModel(
-    imageUrl: 'assets/images/illustration.png',
-    title: 'MUSIC',
-    body: 'EXPERIENCE WICKED PLAYLISTS',
-    titleGradient: <Color>[Color(0xFF9708CC), Color(0xFF43CBBF)],
+    'assets/images/illustration.png',
+    'MUSIC',
+    'EXPERIENCE WICKED PLAYLISTS',
+    <Color>[Color(0xFF9708CC), Color(0xFF43CBBF)],
   ),
   PageModel(
-    imageUrl: 'assets/images/illustration2.png',
-    title: 'YOGA',
-    body: 'FEEL THE MAGIC OF THE WELLNESS',
-    titleGradient: <Color>[Color(0xFFE2859F), Color(0xFFFCCF31)],
+    'assets/images/illustration2.png',
+    'YOGA',
+    'FEEL THE MAGIC OF THE WELLNESS',
+    <Color>[Color(0xFFE2859F), Color(0xFFFCCF31)],
   ),
   PageModel(
-    imageUrl: 'assets/images/illustration3.png',
-    title: 'TRAVEL',
-    body: 'LET\'S WAKE UP AND KNOW THE WORLD',
-    titleGradient: <Color>[Color(0xFF5EFCE8), Color(0xFF736EFE)],
+    'assets/images/illustration3.png',
+    'TRAVEL',
+    'LET\'S WAKE UP AND KNOW THE WORLD',
+    <Color>[Color(0xFF5EFCE8), Color(0xFF736EFE)],
   ),
 ];
