52e0dbc14823cd8b30fd9b0422f38ebcc4436a78
FabianVarela <jfabian.303@gmail.com>
2020-06-14 23:30:20 -0500

    Use the custom hidden menu and modify main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_ui_examples/common/custom_drawer.dart';
+import 'package:flutter_ui_examples/common/custom_hidden_menu.dart';
 import 'package:flutter_ui_examples/common/responsive.dart';
 import 'package:flutter_ui_examples/ui_adidas_ecommerce/adidas.ui.dart';
 import 'package:flutter_ui_examples/ui_furniture/furniture.ui.dart';
@@ -10,7 +11,6 @@ import 'package:flutter_ui_examples/ui_on_boarding/on_boarding.ui.dart';
 import 'package:flutter_ui_examples/ui_shopping/shopping.ui.dart';
 import 'package:flutter_ui_examples/ui_story/story.ui.dart';
 import 'package:flutter_ui_examples/ui_streaming/streaming.ui.dart';
-import 'package:hidden_drawer_menu/hidden_drawer/hidden_drawer_menu.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,7 +26,9 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+class _MyAppState extends State<MyApp> {
+  bool _isExpand;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,23 +38,19 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
         return MaterialPageRoute<dynamic>(
           settings: settings,
           builder: (BuildContext ctx) {
-            final double width = MediaQuery.of(ctx).size.width;
-            final double height = MediaQuery.of(ctx).size.height;
+            Responsive.init(
+              ctx,
+              width: MediaQuery.of(ctx).size.width,
+              height: MediaQuery.of(ctx).size.height,
+              allowFontScaling: true,
+            );
 
-            Responsive.init(ctx,
-                width: width, height: height, allowFontScaling: true);
-
-            return SimpleHiddenDrawer(
-              isDraggable: false,
-              menu: CustomDrawer(),
-              screenSelectedBuilder:
-                  (int position, SimpleHiddenDrawerBloc controller) {
-                if (settings.name == '/' || settings.name == '/login') {
-                  return _setRoute(settings.name);
-                } else {
-                  return _setScreenFromMenu(position, controller);
-                }
-              },
+            return CustomHiddenMenu(
+              isOpen: _isExpand,
+              menu: CustomDrawer(
+                onCloseMenu: () => setState(() => _isExpand = false),
+              ),
+              child: _setRoute(settings.name),
             );
           },
         );
@@ -67,28 +65,29 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
       case '/login':
         return LoginUI();
       case '/shopping':
-        return ShoppingUI(onPressedMenu: () {});
+        return ShoppingUI(onPressedMenu: _openDrawer);
+      case '/story':
+        return StoryUI(onPressedMenu: _openDrawer);
+      case '/music':
+        return MusicUI(onPressedMenu: _openDrawer);
+      case '/streaming':
+        return StreamingUI(onPressedMenu: _openDrawer);
+      case '/furniture':
+        return FurnitureUI(onPressedMenu: _openDrawer);
+      case '/adidas':
+        return AdidasUI(onPressedMenu: _openDrawer);
       default:
         return null;
     }
   }
 
-  Widget _setScreenFromMenu(int position, SimpleHiddenDrawerBloc controller) {
-    switch (position) {
-      case 0:
-        return ShoppingUI(onPressedMenu: controller.toggle);
-      case 1:
-        return StoryUI(onPressedMenu: controller.toggle);
-      case 2:
-        return MusicUI(onPressedMenu: controller.toggle);
-      case 3:
-        return StreamingUI(onPressedMenu: controller.toggle);
-      case 4:
-        return FurnitureUI(onPressedMenu: controller.toggle);
-      case 5:
-        return AdidasUI(onPressedMenu: controller.toggle);
-      default:
-        return null;
-    }
+  void _openDrawer() {
+    setState(() {
+      if (_isExpand ?? false) {
+        _isExpand = false;
+      } else {
+        _isExpand = true;
+      }
+    });
   }
 }
