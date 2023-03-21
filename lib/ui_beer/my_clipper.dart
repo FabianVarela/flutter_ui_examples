8473c44fe2063040e0462638f6b628bc31a84e12
FabianVarela <jfabian.303@gmail.com>
2023-03-21 17:29:39 -0500

    Create custom clipper file

---
+++ lib/ui_beer/my_clipper.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0, size.height - 100);
+
+    final Offset controlPoint = Offset(20, size.height);
+    final Offset endPoint = Offset(size.width / 2, size.height);
+
+    path.quadraticBezierTo(
+        controlPoint.dx, controlPoint.dy, endPoint.dx, endPoint.dy);
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
