e109105c6b0afab90eb292c21dd6adeef86bc43c
FabianVarela <jfabian.303@gmail.com>
2023-03-22 16:34:11 -0500

    Add parameter to redirect from main and set opacity animation to menu

--- lib/common/custom_drawer.dart
+++ lib/common/custom_drawer.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomDrawer extends StatelessWidget {
-  const CustomDrawer({required this.onCloseMenu, super.key});
+  const CustomDrawer({
+    required this.onRedirect,
+    required this.onCloseMenu,
+    this.isShowing = false,
+    super.key,
+  });
 
+  final ValueSetter<String> onRedirect;
   final VoidCallback onCloseMenu;
+  final bool isShowing;
 
   @override
   Widget build(BuildContext context) {
@@ -14,35 +21,39 @@ class CustomDrawer extends StatelessWidget {
         height: double.maxFinite,
         color: Colors.blueGrey,
         padding: const EdgeInsets.all(8),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              _MenuItem(
-                text: 'Shopping UI',
-                onTap: () => _redirectTo(context, '/shopping'),
-              ),
-              _MenuItem(
-                text: 'Story UI',
-                onTap: () => _redirectTo(context, '/story'),
-              ),
-              _MenuItem(
-                text: 'Music UI',
-                onTap: () => _redirectTo(context, '/music'),
-              ),
-              _MenuItem(
-                text: 'Streaming UI',
-                onTap: () => _redirectTo(context, '/streaming'),
-              ),
-              _MenuItem(
-                text: 'Furniture UI',
-                onTap: () => _redirectTo(context, '/furniture'),
-              ),
-              _MenuItem(
-                text: 'Adidas e-commerce UI',
-                onTap: () => _redirectTo(context, '/adidas'),
-              ),
-            ],
+        child: AnimatedOpacity(
+          duration: const Duration(milliseconds: 500),
+          opacity: isShowing ? 1 : 0,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                _MenuItem(
+                  text: 'Shopping UI',
+                  onTap: () => _redirectTo(context, '/shopping'),
+                ),
+                _MenuItem(
+                  text: 'Story UI',
+                  onTap: () => _redirectTo(context, '/story'),
+                ),
+                _MenuItem(
+                  text: 'Music UI',
+                  onTap: () => _redirectTo(context, '/music'),
+                ),
+                _MenuItem(
+                  text: 'Streaming UI',
+                  onTap: () => _redirectTo(context, '/streaming'),
+                ),
+                _MenuItem(
+                  text: 'Furniture UI',
+                  onTap: () => _redirectTo(context, '/furniture'),
+                ),
+                _MenuItem(
+                  text: 'Adidas e-commerce UI',
+                  onTap: () => _redirectTo(context, '/adidas'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -51,7 +62,7 @@ class CustomDrawer extends StatelessWidget {
 
   void _redirectTo(BuildContext context, String route) {
     onCloseMenu();
-    Navigator.of(context).pushReplacementNamed(route);
+    onRedirect(route);
   }
 }
 
