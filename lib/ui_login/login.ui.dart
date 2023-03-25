ff5f7ffebb76a2b39df9f574612da49bd394c523
FabianVarela <jfabian.303@gmail.com>
2023-03-25 14:49:09 -0500

    Modify login.ui.dart screen

--- lib/ui_login/login.ui.dart
+++ lib/ui_login/login.ui.dart
@@ -1,19 +1,14 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_examples/common/responsive.dart';
-import 'package:flutter_ui_examples/ui_login/card_form.dart';
-import 'package:flutter_ui_examples/ui_login/custom_icon.dart';
-import 'package:flutter_ui_examples/ui_login/social_icons.dart';
+import 'package:flutter_ui_examples/ui_login/widget/background_image.dart';
+import 'package:flutter_ui_examples/ui_login/widget/card_form.dart';
+import 'package:flutter_ui_examples/ui_login/widget/sign_in_buttons.dart';
+import 'package:flutter_ui_examples/ui_login/widget/social_buttons.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginUI extends StatefulWidget {
+class LoginUI extends StatelessWidget {
   const LoginUI({super.key});
-
-  @override
-  _LoginUIState createState() => _LoginUIState();
-}
-
-class _LoginUIState extends State<LoginUI> {
-  bool _isSelected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,221 +18,59 @@ class _LoginUIState extends State<LoginUI> {
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          _setBackgroundImages(),
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20, right: 28, top: 60),
-              child: Column(
-                children: <Widget>[
-                  _setLogoImages(),
-                  SizedBox(height: Responsive().setHeight(120)),
-                  const CardForm(),
-                  SizedBox(height: Responsive().setHeight(20)),
-                  _setSignIn(),
-                  SizedBox(height: Responsive().setHeight(30)),
-                  _setSocialTitle(),
-                  SizedBox(height: Responsive().setHeight(30)),
-                  _socialIcons(),
-                  SizedBox(height: Responsive().setHeight(30)),
-                  _setSignUp(),
-                  SizedBox(height: Responsive().setHeight(20))
-                ],
-              ),
+          const BackgroundImage(),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Responsive().setHeight(20),
+              vertical: Responsive().setHeight(60),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/login/login_logo.png',
+                  width: Responsive().setWidth(70),
+                  height: Responsive().setHeight(70),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: Responsive().setHeight(120)),
+                  child: const CardForm(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: Responsive().setHeight(20)),
+                  child: SignInButtons(
+                    onSignIn: () {
+                      Navigator.pushReplacementNamed(context, '/shopping');
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: Responsive().setHeight(30)),
+                  child: const SocialButtons(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: Responsive().setHeight(30)),
+                  child: Align(
+                    child: RichText(
+                      text: TextSpan(
+                        style: GoogleFonts.mulish(color: Colors.black),
+                        children: <TextSpan>[
+                          const TextSpan(text: 'New User? '),
+                          TextSpan(
+                            text: 'Sign Up',
+                            style: const TextStyle(color: Color(0xFF5D74E3)),
+                            recognizer: TapGestureRecognizer()..onTap = () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
       ),
-    );
-  }
-
-  Widget _setBackgroundImages() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: Responsive().setHeight(20)),
-          child: Image.asset('assets/images/login/login_image_01.png'),
-        ),
-        Expanded(child: Container()),
-        Padding(
-          padding: EdgeInsets.only(top: Responsive().setHeight(20)),
-          child: Image.asset('assets/images/login/login_image_02.png'),
-        ),
-      ],
-    );
-  }
-
-  Widget _setLogoImages() {
-    return Row(
-      children: <Widget>[
-        Image.asset(
-          'assets/images/login/login_logo.png',
-          width: Responsive().setWidth(70),
-          height: Responsive().setHeight(70),
-        ),
-        Text(
-          'LOGO',
-          style: GoogleFonts.mulish(
-            fontSize: Responsive().setSp(30),
-            letterSpacing: .6,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _setSignIn() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            const SizedBox(width: 12),
-            GestureDetector(
-              onTap: () => setState(() => _isSelected = !_isSelected),
-              child: _radioButton(_isSelected, 'Remember me'),
-            ),
-          ],
-        ),
-        InkWell(
-          onTap: () => Navigator.pushReplacementNamed(context, '/shopping'),
-          child: Container(
-            width: Responsive().setWidth(200),
-            height: Responsive().setHeight(60),
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                colors: <Color>[Color(0xFF17EAD9), Color(0xFF6078EA)],
-              ),
-              borderRadius: BorderRadius.circular(6),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  color: const Color(0xFF6078EA).withOpacity(.3),
-                  offset: const Offset(0, 8),
-                  blurRadius: 8,
-                ),
-              ],
-            ),
-            child: Center(
-              child: Text(
-                'Sign In',
-                style: GoogleFonts.mulish(
-                  color: Colors.white,
-                  fontSize: Responsive().setSp(18),
-                  letterSpacing: 1,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _radioButton(bool isSelected, String text) {
-    return Row(
-      children: <Widget>[
-        Container(
-          width: 16,
-          height: 16,
-          padding: const EdgeInsets.all(2),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(width: 2),
-          ),
-          child: isSelected
-              ? Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.black,
-                  ),
-                )
-              : Container(),
-        ),
-        const SizedBox(width: 8),
-        Text(
-          text,
-          style: GoogleFonts.mulish(fontSize: 12),
-        ),
-      ],
-    );
-  }
-
-  Widget _setSocialTitle() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        _horizontalLine(),
-        Text(
-          'Social login',
-          style: GoogleFonts.mulish(fontSize: 16),
-        ),
-        _horizontalLine()
-      ],
-    );
-  }
-
-  Widget _horizontalLine() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Container(
-        width: Responsive().setWidth(85),
-        height: 1,
-        color: Colors.black26.withOpacity(.2),
-      ),
-    );
-  }
-
-  Widget _socialIcons() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        SocialIcons(
-          colors: const <Color>[
-            Color(0xFF102397),
-            Color(0xFF187ADF),
-            Color(0xFF00EAF8),
-          ],
-          iconData: CustomIcon.facebook,
-          onPressed: () {},
-        ),
-        SocialIcons(
-          colors: const <Color>[Color(0xFFFF4F38), Color(0xFFFF355D)],
-          iconData: CustomIcon.google,
-          onPressed: () {},
-        ),
-        SocialIcons(
-          colors: const <Color>[Color(0xFF17EAD9), Color(0xFF6078EA)],
-          iconData: CustomIcon.twitter,
-          onPressed: () {},
-        ),
-        SocialIcons(
-          colors: const <Color>[Color(0xFF00C6FB), Color(0xFF005BEA)],
-          iconData: CustomIcon.linkedin,
-          onPressed: () {},
-        ),
-      ],
-    );
-  }
-
-  Widget _setSignUp() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          'New User? ',
-          style: GoogleFonts.mulish(),
-        ),
-        InkWell(
-          onTap: () {},
-          child: Text(
-            'Sign Up',
-            style: GoogleFonts.mulish(color: const Color(0xFF5D74E3)),
-          ),
-        ),
-      ],
     );
   }
 }
