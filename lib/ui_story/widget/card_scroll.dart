af687f2a53f4fb229247816352e248a9ebbb3cbe
FabianVarela <jfabian.303@gmail.com>
2023-03-25 12:55:26 -0500

    Move existent widgets to a new folder

---
+++ lib/ui_story/widget/card_scroll.dart
@@ -0,0 +1,145 @@
+import 'dart:math';
+
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_examples/common/responsive.dart';
+import 'package:flutter_ui_examples/ui_story/model/story_model.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double _cardAspectRatio = 12.0 / 16.0;
+
+class CardScroll extends StatelessWidget {
+  const CardScroll({
+    required this.stories,
+    required this.currentPage,
+    this.padding = 20,
+    this.verticalInset = 20,
+    super.key,
+  });
+
+  final List<StoryModel> stories;
+  final double currentPage;
+  final double padding;
+  final double verticalInset;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: _cardAspectRatio * 1.2,
+      child: LayoutBuilder(
+        builder: (_, constraint) {
+          final width = constraint.maxWidth;
+          final height = constraint.maxHeight;
+
+          final safeWidth = width - 2 * padding;
+          final safeHeight = height - 2 * padding;
+
+          final heightOfPrimaryCard = safeHeight;
+          final widthOfPrimaryCard = heightOfPrimaryCard * _cardAspectRatio;
+
+          final primaryCardLeft = safeWidth - widthOfPrimaryCard;
+          final horizontalInset = primaryCardLeft / 2;
+
+          return Stack(
+            children: stories.mapIndexed((index, item) {
+              final delta = index - currentPage;
+              final totalDelta = -delta * (delta > 0 ? 15 : 1);
+
+              final primary = primaryCardLeft - horizontalInset * totalDelta;
+              final start = padding + max(primary, 0);
+
+              return Positioned.directional(
+                start: start,
+                textDirection: TextDirection.rtl,
+                top: padding + verticalInset * max(-delta, 0.0),
+                bottom: padding + verticalInset * max(-delta, 0.0),
+                child: _CardItem(image: item.image, name: item.name),
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _CardItem extends StatelessWidget {
+  const _CardItem({required this.image, required this.name});
+
+  final String image;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: DecoratedBox(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(3, 6),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: AspectRatio(
+          aspectRatio: _cardAspectRatio,
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Image.asset(image, fit: BoxFit.cover),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: Responsive().setWidth(16),
+                        vertical: Responsive().setHeight(8),
+                      ),
+                      child: Text(
+                        name,
+                        style: GoogleFonts.varelaRound(
+                          color: Colors.white,
+                          fontSize: Responsive().setSp(25),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: Responsive().setWidth(12),
+                        bottom: Responsive().setHeight(12),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size.zero,
+                          backgroundColor: Colors.blueAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: Responsive().setWidth(22),
+                            vertical: Responsive().setHeight(6),
+                          ),
+                        ),
+                        child: Text(
+                          'Read later',
+                          style: GoogleFonts.varelaRound(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
