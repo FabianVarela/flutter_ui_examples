4d818f9a80f1d1c8907eb91e05a28c639c7b994c
FabianVarela <jfabian.303@gmail.com>
2023-03-27 22:47:52 -0500

    Rename beer_ui.dart file and add to router

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_ui_examples/common/menu/custom_drawer.dart';
 import 'package:flutter_ui_examples/common/menu/custom_hidden_menu.dart';
 import 'package:flutter_ui_examples/common/responsive.dart';
 import 'package:flutter_ui_examples/ui_adidas_ecommerce/adidas_ui.dart';
+import 'package:flutter_ui_examples/ui_beer/beer_ui.dart';
 import 'package:flutter_ui_examples/ui_furniture/furniture_ui.dart';
 import 'package:flutter_ui_examples/ui_login/login_ui.dart';
 import 'package:flutter_ui_examples/ui_music/music_ui.dart';
@@ -76,6 +77,8 @@ class _MyAppState extends State<MyApp> {
         return FurnitureUI(onPressedMenu: _openDrawer);
       case '/adidas':
         return AdidasUI(onPressedMenu: _openDrawer);
+      case '/beer':
+        return BeerUI(onPressedMenu: _openDrawer);
       case '/':
       default:
         return const OnBoardingUI();
