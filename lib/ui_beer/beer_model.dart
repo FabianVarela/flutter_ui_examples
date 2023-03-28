59c9bc53ab6440333673a51c1a79a79521d32143
FabianVarela <jfabian.303@gmail.com>
2023-03-27 22:43:36 -0500

    Modify beer_model.dart structure

--- lib/ui_beer/beer_model.dart
+++ lib/ui_beer/beer_model.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 class BeerModel {
+  BeerModel({
+    required this.name,
+    required this.slogan,
+    required this.description,
+    required this.rating,
+    required this.imageLogo,
+    required this.bottleLogo,
+    required this.color,
+    required this.textColor,
+  });
+
   final String name;
   final String slogan;
   final String description;
@@ -9,88 +20,87 @@ class BeerModel {
   final String bottleLogo;
   final Color color;
   final Color textColor;
-
-  BeerModel(
-    this.name,
-    this.slogan,
-    this.description,
-    this.rating,
-    this.imageLogo,
-    this.bottleLogo,
-    this.color,
-    this.textColor,
-  );
 }
 
-List<BeerModel> beers = <BeerModel>[
+final beers = <BeerModel>[
   BeerModel(
-    'Aguila Original',
-    'Colombian Pilsner & Pure Tradition',
-    'Aguila Original, classic, tasty and refreshing lager beer than has been enjoyed for more than a century.',
-    5,
-    'assets/images/beer/aguila_logo.png',
-    'assets/images/beer/aguila.png',
-    Colors.transparent,
-    Colors.transparent,
+    name: 'Aguila Original',
+    slogan: 'Colombian Pilsner & Pure Tradition',
+    description:
+        'Aguila Original, classic, tasty and refreshing lager beer than has '
+        'been enjoyed for more than a century.',
+    rating: 5,
+    imageLogo: 'assets/images/beer/aguila_logo.png',
+    bottleLogo: 'assets/images/beer/aguila.png',
+    color: Colors.transparent,
+    textColor: Colors.transparent,
   ),
   BeerModel(
-    'Budweiser',
-    'King Of Beers',
-    'Introduced by Adolphus Busch in 1876 and is still produced with the same care and high quality, extracting standards.',
-    4.1,
-    'assets/images/beer/budweiser_logo.png',
-    'assets/images/beer/budweiser.png',
-    Colors.transparent,
-    Colors.transparent,
+    name: 'Budweiser',
+    slogan: 'King Of Beers',
+    description:
+        'Introduced by Adolphus Busch in 1876 and is still produced with the '
+        'same care and high quality, extracting standards.',
+    rating: 4.1,
+    imageLogo: 'assets/images/beer/budweiser_logo.png',
+    bottleLogo: 'assets/images/beer/budweiser.png',
+    color: Colors.transparent,
+    textColor: Colors.transparent,
   ),
   BeerModel(
-    'Club Colombia Dorada',
-    'Colombian Lager & Pure Tradition',
-    'Born in 1949 in Colombia with the name of Club Sixty, in commemoration of the sixty years of the Bavaria foundation.',
-    3.9,
-    'assets/images/beer/club_colombia_logo.png',
-    'assets/images/beer/club_colombia_dorada.png',
-    Colors.transparent,
-    Colors.transparent,
+    name: 'Club Colombia\nDorada',
+    slogan: 'Colombian Lager & Pure Tradition',
+    description: 'Born in 1949 in Colombia with the name of Club Sixty, in '
+        'commemoration of the sixty years of the Bavaria foundation.',
+    rating: 3.9,
+    imageLogo: 'assets/images/beer/club_colombia_logo.png',
+    bottleLogo: 'assets/images/beer/club_colombia_dorada.png',
+    color: Colors.transparent,
+    textColor: Colors.transparent,
   ),
   BeerModel(
-    'Club Colombia Roja',
-    'Colombian Lager & Pure Tradition',
-    'Born in 1949 in Colombia with the name of Club Sixty, in commemoration of the sixty years of the Bavaria foundation.',
-    3.5,
-    'assets/images/beer/club_colombia_logo.png',
-    'assets/images/beer/club_colombia_roja.png',
-    Colors.transparent,
-    Colors.transparent,
+    name: 'Club Colombia\nRoja',
+    slogan: 'Colombian Lager & Pure Tradition',
+    description: 'Born in 1949 in Colombia with the name of Club Sixty, in '
+        'commemoration of the sixty years of the Bavaria foundation.',
+    rating: 3.5,
+    imageLogo: 'assets/images/beer/club_colombia_logo.png',
+    bottleLogo: 'assets/images/beer/club_colombia_roja.png',
+    color: Colors.transparent,
+    textColor: Colors.transparent,
   ),
   BeerModel(
-    'Club Colombia Negra',
-    'Colombian Lager & Pure Tradition',
-    'Born in 1949 in Colombia with the name of Club Sixty, in commemoration of the sixty years of the Bavaria foundation.',
-    4.5,
-    'assets/images/beer/club_colombia_logo.png',
-    'assets/images/beer/club_colombia_negra.png',
-    Colors.transparent,
-    Colors.transparent,
+    name: 'Club Colombia\nNegra',
+    slogan: 'Colombian Lager & Pure Tradition',
+    description: 'Born in 1949 in Colombia with the name of Club Sixty, in '
+        'commemoration of the sixty years of the Bavaria foundation.',
+    rating: 4.5,
+    imageLogo: 'assets/images/beer/club_colombia_logo.png',
+    bottleLogo: 'assets/images/beer/club_colombia_negra.png',
+    color: Colors.transparent,
+    textColor: Colors.transparent,
   ),
   BeerModel(
-    'Club Colombia Trigo',
-    'Colombian Lager & Pure Tradition',
-    'Born in 1949 in Colombia with the name of Club Sixty, in commemoration of the sixty years of the Bavaria foundation.',
-    4.3,
-    'assets/images/beer/club_colombia_logo.png',
-    'assets/images/beer/club_colombia_trigo.png',
-    Colors.transparent,
-    Colors.transparent,
+    name: 'Club Colombia Trigo',
+    slogan: 'Colombian Lager & Pure Tradition',
+    description: 'Born in 1949 in Colombia with the name of Club Sixty, in '
+        'commemoration of the sixty years of the Bavaria foundation.',
+    rating: 4.3,
+    imageLogo: 'assets/images/beer/club_colombia_logo.png',
+    bottleLogo: 'assets/images/beer/club_colombia_trigo.png',
+    color: Colors.transparent,
+    textColor: Colors.transparent,
   ),
   BeerModel(
-    'Stella Artois',
-    'The Original Belgium Lager',
-    'Belgian pilsner of between 4.8 and 5.2 percent ABV which was first brewed by Brouwerij Artois.',
-    5,
-    'assets/images/beer/stella_artois_logo.png',
-    'assets/images/beer/stella_artois.png',
-    Colors.transparent,
-    Colors.transparent,
+    name: 'Stella Artois',
+    slogan: 'The Original Belgium Lager',
+    description:
+        'Belgian pilsner of between 4.8 and 5.2 percent ABV which was first '
+        'brewed by Brouwerij Artois.',
+    rating: 5,
+    imageLogo: 'assets/images/beer/stella_artois_logo.png',
+    bottleLogo: 'assets/images/beer/stella_artois.png',
+    color: Colors.transparent,
+    textColor: Colors.transparent,
   ),
 ];
