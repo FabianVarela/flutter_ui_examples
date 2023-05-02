da9899e46fb2406c0946a9a5665f1a37bdc7bd9d
FabianVarela <jfabian.303@gmail.com>
2023-05-01 19:14:17 -0500

    Create beer_header.dart using animation logo

---
+++ lib/ui_beer/widget/beer_header.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_examples/common/responsive.dart';
+import 'package:flutter_ui_examples/ui_beer/model/beer_model.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BeerHeader extends StatefulWidget {
+  const BeerHeader({required this.index, super.key});
+
+  final int index;
+
+  @override
+  State<BeerHeader> createState() => _BeerHeaderState();
+}
+
+class _BeerHeaderState extends State<BeerHeader> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  late int _imageIndex = widget.index;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _animation = Tween<Offset>(begin: Offset.zero, end: const Offset(0, 1))
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant BeerHeader oldWidget) {
+    if (oldWidget.index != widget.index) {
+      _controller.forward();
+
+      Future.delayed(const Duration(milliseconds: 800), () {
+        setState(() => _imageIndex = widget.index);
+        _controller.reverse();
+      });
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top: 130, left: 40, right: 20),
+          child: AnimatedDefaultTextStyle(
+            curve: Curves.fastOutSlowIn,
+            duration: const Duration(seconds: 1),
+            style: GoogleFonts.montserrat(
+              fontSize: Responsive().setSp(30),
+              fontWeight: FontWeight.w700,
+              color: beers[widget.index].textColor,
+            ),
+            child: const Text('Grab\nyour\nbeer.'),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: FractionalTranslation(
+            translation: const Offset(0, .25),
+            child: SlideTransition(
+              position: _animation,
+              child: Opacity(
+                opacity: .15,
+                child: Image.asset(beers[_imageIndex].imageLogo),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
