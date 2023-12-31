1b31468ffeda34a81e5adccee94c1a27f9e2f8c2
FabianVarela <jfabian.303@gmail.com>
2023-12-30 21:53:35 -0500

    Add new elements to the list

--- lib/ui_streaming/model/streaming_model.dart
+++ lib/ui_streaming/model/streaming_model.dart
@@ -7,10 +7,29 @@ class StreamingModel {
 
 final streamingList = <StreamingModel>[
   StreamingModel(
+    'assets/images/streaming/jujutsu.jpeg',
+    'Jujutsu Kaisen',
+  ),
+  StreamingModel(
+    'assets/images/streaming/mandalorian.png',
+    'The Mandalorian',
+  ),
+  StreamingModel(
+    'assets/images/streaming/succession.jpg',
+    'Succession',
+  ),
+  StreamingModel(
+    'assets/images/streaming/theBear.jpg',
+    'The bear',
+  ),
+  StreamingModel(
     'assets/images/streaming/fullMetalAlchemist.jpg',
     'Fullmetal alchemist: Brotherhood',
   ),
-  StreamingModel('assets/images/streaming/daredevil.jpg', 'Daredevil'),
+  StreamingModel(
+    'assets/images/streaming/daredevil.jpg',
+    'Daredevil',
+  ),
   StreamingModel(
     'assets/images/streaming/avengers.jpeg',
     'Avengers: Infinity war',
@@ -19,7 +38,16 @@ final streamingList = <StreamingModel>[
     'assets/images/streaming/batman.jpg',
     'Batman: The dark knight',
   ),
-  StreamingModel('assets/images/streaming/onePunchMan.png', 'One punch man'),
-  StreamingModel('assets/images/streaming/breakingBad.jpg', 'Breaking bad'),
-  StreamingModel('assets/images/streaming/dark.jpg', 'Dark'),
+  StreamingModel(
+    'assets/images/streaming/onePunchMan.png',
+    'One punch man',
+  ),
+  StreamingModel(
+    'assets/images/streaming/breakingBad.jpg',
+    'Breaking bad',
+  ),
+  StreamingModel(
+    'assets/images/streaming/dark.jpg',
+    'Dark',
+  ),
 ];
