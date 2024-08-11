bc0b0def5c5503ad3c23f7442b641a0b4d45603f
FabianVarela <jfabian.303@gmail.com>
2024-08-11 17:42:11 -0500

    Create ice cream ui screen

---
+++ lib/ui_ice_cream/ice_cream_ui.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_ui_examples/common/gen/assets.gen.dart';
+import 'package:flutter_ui_examples/common/gen/fonts.gen.dart';
+import 'package:gap/gap.dart';
+
+class IceCreamUI extends HookWidget {
+  const IceCreamUI({required this.onPressedMenu, super.key});
+
+  final VoidCallback onPressedMenu;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF593AF),
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              const Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    'Ice Cream\nShop',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 48,
+                      color: Colors.white,
+                      fontFamily: FontFamily.ritts,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const Gap(50),
+              Expanded(
+                flex: 2,
+                child: FractionalTranslation(
+                  translation: const Offset(-.05, 0),
+                  child: Transform.flip(
+                    flipX: true,
+                    child: Assets.images.iceCream.iceCreamChocolate.image(
+                      height: MediaQuery.sizeOf(context).height,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            right: 30,
+            bottom: 30,
+            child: Row(
+              children: <Widget>[
+                const Text(
+                  'Explore',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xFFFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const Gap(16),
+                SizedBox.fromSize(
+                  size: const Size(60, 60),
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFF01359),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Assets.images.iceCream.arrowLeft.svg(
+                        colorFilter: const ColorFilter.mode(
+                          Color(0xFFFFFFFF),
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
