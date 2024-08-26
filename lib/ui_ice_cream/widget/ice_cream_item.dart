cfba23cc24d1cf1adc84844222da749b6d746ff3
FabianVarela <jfabian.303@gmail.com>
2024-08-26 07:28:25 -0500

    Create blank ice cream item list

---
+++ lib/ui_ice_cream/widget/ice_cream_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_examples/ui_ice_cream/model/ice_cream_model.dart';
+
+class IceCreamItem extends StatelessWidget {
+  const IceCreamItem({required this.iceCream, super.key});
+
+  final IceCreamModel iceCream;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
