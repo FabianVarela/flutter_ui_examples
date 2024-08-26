d274fd91078e762a61f97b5d299f4cad283ff2d3
FabianVarela <jfabian.303@gmail.com>
2024-08-26 12:05:43 -0500

    Add background and foreground color

--- lib/ui_ice_cream/model/ice_cream_model.dart
+++ lib/ui_ice_cream/model/ice_cream_model.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter_ui_examples/common/gen/assets.gen.dart';
 
 class IceCreamModel {
@@ -6,6 +8,8 @@ class IceCreamModel {
     required this.price,
     required this.rate,
     required this.image,
+    required this.background,
+    required this.foreground,
     this.energy = 0,
     this.calories = 0,
     this.calcium = 0,
@@ -14,8 +18,10 @@ class IceCreamModel {
 
   final String name;
   final double price;
-  final int rate;
+  final double rate;
   final String image;
+  final Color background;
+  final Color foreground;
   final int energy;
   final int calories;
   final double calcium;
@@ -26,8 +32,10 @@ final iceCreamList = <IceCreamModel>[
   IceCreamModel(
     name: 'Strawberry',
     price: 5.89,
-    rate: 4,
+    rate: 3.5,
     image: Assets.images.iceCream.iceCreamStraw.path,
+    background: const Color(0xFFCFB091),
+    foreground: const Color(0xFF6A1b03),
     energy: 1200,
     calories: 150,
     calcium: 20,
@@ -37,6 +45,8 @@ final iceCreamList = <IceCreamModel>[
     price: 6.5,
     rate: 5,
     image: Assets.images.iceCream.iceCreamPistachio.path,
+    background: const Color(0xFFA7D685),
+    foreground: const Color(0xFF2D6E02),
     energy: 1800,
     calories: 100,
     calcium: 40,
