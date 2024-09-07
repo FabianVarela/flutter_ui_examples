bed22dddf0401fccc067c93218ababeeb634e3aa
FabianVarela <jfabian.303@gmail.com>
2024-09-06 21:50:43 -0500

    Implement ice cream detail screen

--- lib/ui_ice_cream/ice_cream_detail_ui.dart
+++ lib/ui_ice_cream/ice_cream_detail_ui.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_ui_examples/common/gen/assets.gen.dart';
+import 'package:flutter_ui_examples/common/gen/fonts.gen.dart';
 import 'package:flutter_ui_examples/ui_ice_cream/model/ice_cream_model.dart';
 import 'package:flutter_ui_examples/ui_ice_cream/widget/option_button.dart';
+import 'package:gap/gap.dart';
 
 class IceCreamDetailArguments {
   IceCreamDetailArguments(this.iceCream);
@@ -9,18 +12,22 @@ class IceCreamDetailArguments {
   final IceCreamModel iceCream;
 }
 
-class IceCreamDetailUI extends StatelessWidget {
+class IceCreamDetailUI extends HookWidget {
   const IceCreamDetailUI({required this.iceCream, super.key});
 
   final IceCreamModel iceCream;
 
   @override
   Widget build(BuildContext context) {
+    final totalValue = useState<double>(0);
+
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -38,10 +45,133 @@ class IceCreamDetailUI extends StatelessWidget {
                   ),
                 ],
               ),
+              const Gap(20),
+              _IceCreamName(name: iceCream.name),
+              const Gap(10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  _IceCreamDescription(iceCream: iceCream),
+                ],
+              ),
+              const Gap(10),
+              _IceCreamTotal(totalValue: totalValue.value),
+              const Gap(20),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _IceCreamName extends StatelessWidget {
+  const _IceCreamName({required this.name});
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '$name\nIce Cream',
+      style: const TextStyle(
+        fontSize: 30,
+        color: Color(0xFFF593AF),
+        fontFamily: FontFamily.ritts,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class _IceCreamDescription extends StatelessWidget {
+  const _IceCreamDescription({required this.iceCream});
+
+  final IceCreamModel iceCream;
+
+  @override
+  Widget build(BuildContext context) {
+    final calciumString = iceCream.calcium.toStringAsFixed(0);
+    final descriptionList = <({String value, String symbol, String text})>[
+      (value: '${iceCream.energy}', symbol: 'KL', text: 'Energy'),
+      (value: '${iceCream.calories}', symbol: 'KCal', text: 'Calories'),
+      (value: calciumString, symbol: '%', text: 'Calcium'),
+      (value: '${iceCream.sugar}', symbol: '%', text: 'Sugar'),
+    ];
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        for (final (index, item) in descriptionList.indexed) ...[
+          RichText(
+            text: TextSpan(
+              text: item.value,
+              style: const TextStyle(
+                fontSize: 20,
+                color: Color(0xFFF11359),
+                fontFamily: FontFamily.museoSansCyrl,
+                fontWeight: FontWeight.w600,
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                  text: item.symbol,
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Color(0xFFF11359),
+                    fontFamily: FontFamily.museoSansCyrl,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text(
+            item.text,
+            style: const TextStyle(
+              fontSize: 12,
+              color: Colors.grey,
+              fontFamily: FontFamily.museoSansCyrl,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+          if (index <= descriptionList.length - 1) const Gap(20),
+        ],
+      ],
+    );
+  }
+}
+
+class _IceCreamTotal extends StatelessWidget {
+  const _IceCreamTotal({required this.totalValue});
+
+  final double totalValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text(
+          'Total:',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.grey,
+            fontFamily: FontFamily.museoSansCyrl,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        Text(
+          '\$ ${totalValue.toStringAsFixed(1)}',
+          style: const TextStyle(
+            fontSize: 30,
+            color: Color(0xFFF11359),
+            fontFamily: FontFamily.museoSansCyrl,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
     );
   }
 }
