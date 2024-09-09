9a0b4a9025a11945f9ef697f9bf3a66568b32b25
FabianVarela <jfabian.303@gmail.com>
2024-09-09 13:00:06 -0500

    Set opacity animation for small logo

--- lib/ui_beer/widget/beer_bottle_item.dart
+++ lib/ui_beer/widget/beer_bottle_item.dart
@@ -3,50 +3,62 @@ part of '../beer_ui.dart';
 class BeerBottleItem extends StatelessWidget {
   const BeerBottleItem({
     required this.beer,
-    required this.controller,
+    required this.pageController,
     this.index = 0,
     super.key,
   });
 
   final BeerModel beer;
-  final PageController controller;
+  final PageController pageController;
   final int index;
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (_, child) {
-        var value = 1.0;
-        if (controller.position.haveDimensions) {
-          value = (controller.page ?? 0) - index;
-          value = Curves.easeOut.transform(
-            (1 - (value.abs() * .5)).clamp(0.0, 1.0),
-          );
-        }
+    return Stack(
+      clipBehavior: Clip.none,
+      children: <Widget>[
+        Positioned(
+          top: 10,
+          right: -30,
+          child: AnimatedBuilder(
+            animation: pageController,
+            builder: (_, __) {
+              var delta = 1.0;
+              if (pageController.position.haveDimensions) {
+                final resultPage = (pageController.page ?? 0) - index;
+                delta = 1 - resultPage.abs().clamp(0.0, 1.0);
+              }
 
-        return Stack(
-          clipBehavior: Clip.none,
-          children: <Widget>[
-            // TODO(FV): Review logo animation
-            Positioned(
-              right: -30,
-              child: Container(
-                width: 40,
-                height: 60,
-                color: index.isOdd ? Colors.red : Colors.blue,
-              ),
-            ),
-            Center(
+              return Opacity(
+                opacity: delta,
+                child: SizedBox.fromSize(
+                  size: const Size(70, 40),
+                  child: Image.asset(beer.smallImage, fit: BoxFit.contain),
+                ),
+              );
+            },
+          ),
+        ),
+        AnimatedBuilder(
+          animation: pageController,
+          builder: (_, __) {
+            var value = 1.0;
+            if (pageController.position.haveDimensions) {
+              value = (pageController.page ?? 0) - index;
+              value = Curves.easeOut.transform(
+                (1 - (value.abs() * .5)).clamp(0, 1),
+              );
+            }
+
+            return Center(
               child: SizedBox.fromSize(
                 size: Size(250, value * 350),
-                child: child,
+                child: Image.asset(beer.bottleImage, fit: BoxFit.fitHeight),
               ),
-            ),
-          ],
-        );
-      },
-      child: Image.asset(beer.bottleImage, fit: BoxFit.fitHeight),
+            );
+          },
+        ),
+      ],
     );
   }
 }
